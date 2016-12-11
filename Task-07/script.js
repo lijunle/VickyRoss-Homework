0ab2a2a4e88c32c7696db137143c2e43c744a8e1
黎凯琦 <343216476@qq.com>
2016-12-11 23:31:12 +0800

    Update script.js

--- Task-07/script.js
+++ Task-07/script.js
@@ -1,41 +1,37 @@
 /****要求一*****/
-function myfuntion1(){
-  document.getElementById("mybtn1").innerHTML="已点击";
+function myfunction1(){
+  document.getElementById("mybtn1").innerHTML = "已点击";
 }
 
 /****要求二*****/
-function myfuntion2(){ 
+function myfunction2(){ 
   document.body.style.background = "yellow";
 }
 
 /****要求三*****/
 function generateRandomColor(){
-  var Arr = new Array();
-  var x = Math.round(Math.random()*100);
-  var y = Math.round(Math.random()*100);
-  var z = Math.round(Math.random()*100);
-  if(x <= 255 && y <= 255 && z <= 255){
-    Arr[0]=x;
-    Arr[1]=y;
-    Arr[2]=z;
-    return Arr;
-  }
-  else{
-    generateRandomColor();
-  }
+  var rgb = [];
+  var x = Math.round(Math.random()*255);
+  var y = Math.round(Math.random()*255);
+  var z = Math.round(Math.random()*255);
+    rgb[0] = x;
+    rgb[1] = y;
+    rgb[2] = z;
+    return rgb;
+  
 }
-function myfuntion3(){ 
+function myfunction3(){ 
   var arr = generateRandomColor();
   document.body.style.background = "rgb(" + arr[0] + "," + arr[1] + "," + arr[2] + ")";
 }
 
 /****要求四*****/
-var i;
 function mOver(obj){
-  i = setInterval(myfuntion3,1000);
+  var i = setInterval(myfuntion3,1000);
+  obj.dataset.setInterval=i;
 }
 function mOut(obj){
-  clearInterval(i);
+  clearInterval(obj.dataset.setInterval);
   document.body.style.background = "#fff";
 }
 
@@ -44,12 +40,12 @@ function mOut(obj){
 function isColorHexCode(a){
   if((a[0] === "#" && (a.length === 7||a.length === 4))||
      (a[0] != "#" && (a.length === 6||a.length === 3))){
-    for(var i=1;i<a.length;i++){
+    for(var i=0;i<a.length;i++){
       if(a[i]!='1'&&a[i]!='2'&&a[i]!='3'&&a[i]!='4'&&a[i]!='5'&&
          a[i]!='6'&&a[i]!='7'&&a[i]!='8'&&a[i]!='9'&&a[i]!='0'&&
          a[i]!='a'&&a[i]!='b'&&a[i]!='c'&&a[i]!='d'&&a[i]!='e'&&
          a[i]!='f'&&a[i]!='A'&&a[i]!='B'&&a[i]!='C'&&a[i]!='D'&&
-         a[i]!='E'&&a[i]!='F'){
+         a[i]!='E'&&a[i]!='F'&&a[i]!='#'){
         return false;
       }
     }
@@ -75,7 +71,7 @@ function changeColor(x){
     }
     else{
       element.classList.add("wronginput");
-      document.body.style.background="#fff";
+      document.body.style.background = "#fff";
     }
   }
   else{
