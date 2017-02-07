162085e4c57a80d1bd560ba46c2665724e5e428e
黎凯琦 <343216476@qq.com>
2017-02-07 14:37:48 +0800

    Update script.js

--- Task-12/script.js
+++ Task-12/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
-  $("#only-btn").click(function(){
-    $.getJSON("http://vicky-php.azurewebsites.net/task-12.json",function(result){
-		for(var i = 0; i < result.Task.length; i++){
-			$.each(result.Task[i], function(key, value){
-				$("#text").append("<li>" + key + "<ul>" + "<li>" + value + "</li>" + "</ul>" + "</li>");
-			  });
-		}  
-    });
-  });
+	$("#only-btn").click(getJson);
+	
+	/**getJson文件**/
+	function getJson(){
+		$.getJSON("http://vicky-php.azurewebsites.net/task-12.json")
+		.done(function(result){
+			for(var i = 0; i < result.Task.length; i++){
+				$.each(result.Task[i], function(key, value){
+					$("#text").append("<li>" + key + "<ul>" + "<li>" + value + "</li>" + "</ul>" + "</li>");
+			    });
+			}
+		});
+	}
+	
 });
