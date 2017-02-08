9bc88ba99655a1c3ca4aca3128de1ebb4a835e20
黎凯琦 <343216476@qq.com>
2017-02-08 22:42:18 +0800

    Update script.js

--- Task-12/script.js
+++ Task-12/script.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
-	$("#only-btn").click(getJson);
+	// 获取模板中定义的HTML结构
+	var source = $("#tutorial-template").html();
+	// 编译HTML生成对应的JS模板
+	var template = Handlebars.compile(source);
 	
+	$("#only-btn").click(getJson);
 	/**getJson文件**/
 	function getJson(){
-		
-		
-		$.getJSON("http://vicky-php.azurewebsites.net/task-12.json")
+		$.getJSON("task-12.json")
 		.done(function(result){
-			var context = new Array();
-			context = result;
-			
-			// 获取模板中定义的HTML结构
-			var source = $("#tutorial-template").html();
-			// 编译HTML生成对应的JS模板
-			var template = Handlebars.compile(source);
+			var context = result;
 			// 使用JS模板处理需要显示的JSON数据, 生成对应的HTML内容
 			var html = template(context);
 			$("#text").html(html);
