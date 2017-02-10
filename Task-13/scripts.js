4dfd6242a8f3516f48b6f3ca46c76f707ec9aa5e
黎凯琦 <343216476@qq.com>
2017-02-10 17:31:33 +0800

    Update scripts.js

--- Task-13/scripts.js
+++ Task-13/scripts.js
@@ -1,33 +1,22 @@
 $("form").submit(function(e){
   e.preventDefault();
+  searchMusic();
 });
 var searchBtn = $("#search-btn");
 var search = $("#search");
-var title = $("#title");
-title.hover(function(){
-  title.addClass("infinite");
-},function(){
-  title.removeClass("infinite");
-});
-search.focus(function(){
-  search.addClass("focus-search");
-});
-search.blur(function(){
-  search.removeClass("focus-search");
-});
-
-  searchBtn.click(searchMusic);
-  function searchMusic(){
-    $("#spinner").addClass("fa fa-spinner fa-pulse fa-3x fa-fw");
-    var val = search.val();
-    $.getJSON("https://api.douban.com/v2/music/search?callback=?&q=" + val)
-    .done(function(data){
-      var context = data;
-      console.log(context.musics);
-      var source = $("#musics-template").html();
-      var template = Handlebars.compile(source);
-      var html = template(context);
-      $("#search-reslut").html(html);
-      $("#spinner").hide();
-    });
-  }
+var spinner = $("#spinner");
+var source = $("#musics-template").html();
+var template = Handlebars.compile(source);
+spinner.hide();
+function searchMusic(){
+  spinner.show();
+  var val = search.val();
+  $.getJSON("https://api.douban.com/v2/music/search?callback=?&q=" + val)
+  .done(function(data){
+    var context = data;
+    console.log(context.musics);
+    var html = template(context);
+    $("#search-reslut").html(html);
+    $("#spinner").hide();
+  });
+}
