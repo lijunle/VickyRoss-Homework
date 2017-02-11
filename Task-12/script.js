33b145953e7b8bcb32861f92875c7737a0a863e9
黎凯琦 <343216476@qq.com>
2017-02-11 19:42:34 +0800

    Update script.js

--- Task-12/script.js
+++ Task-12/script.js
@@ -4,25 +4,34 @@ $(document).ready(function(){
 	// 编译HTML生成对应的JS模板
 	var template = Handlebars.compile(source);
 	var onlyBtn = $("#only-btn");
-	onlyBtn.click(getJson);
 	/**getJson文件**/
 	function getJson(){
 		onlyBtn.attr("disabled","true");
+		var def = $.Deferred();
 		$.getJSON("task-12.json")
 		.then(function(result){
 			var context = result;
 			// 使用JS模板处理需要显示的JSON数据, 生成对应的HTML内容
 			var html = template(context);
 			$("#text").html(html);
+			def.resolve();
 		})
-		.then(function(){
-			var def = $.Deferred();
-			setTimeout(function(){ def.resolve(); },2000);
-			return def.promise();
-		})
-		.then(function(){
-			onlyBtn.removeAttr("disabled");
-		})
+		return def.promise();
 	}
+	function afterTwoSecond(){
+		var def = $.Deferred();
+		setTimeout(function(){ def.resolve(); },2000);
+		return def.promise();
+	}
+	function enableButton(){
+		onlyBtn.removeAttr("disabled");
+	}
+		
+		onlyBtn.click(function(){
+			getJson()
+			.then(afterTwoSecond)
+			.then(enableButton)
+		});
+		
 	
 });
