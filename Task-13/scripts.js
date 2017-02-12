a977d7c8d04a9eccef26d59310d2ed2143e824ee
黎凯琦 <343216476@qq.com>
2017-02-12 15:03:54 +0800

    Update scripts.js

--- Task-13/scripts.js
+++ Task-13/scripts.js
@@ -1,22 +1,32 @@
-$("form").submit(function(e){
-  e.preventDefault();
-  searchMusic();
-});
 var searchBtn = $("#search-btn");
 var search = $("#search");
 var spinner = $("#spinner");
 var source = $("#musics-template").html();
 var template = Handlebars.compile(source);
+var searchResult = $("#search-reslut");
+var noResult = $("#no-result");
 spinner.hide();
+
+$("form").submit(function(e){
+  e.preventDefault();
+  searchMusic();
+});
 function searchMusic(){
+  noResult.hide();
   spinner.show();
+  searchResult.html("");
   var val = search.val();
   $.getJSON("https://api.douban.com/v2/music/search?callback=?&q=" + val)
   .done(function(data){
     var context = data;
     console.log(context.musics);
-    var html = template(context);
-    $("#search-reslut").html(html);
+    if(context.musics.length === 0){
+      searchResult.html("find nothing...X_X");
+    }
+    else{
+      var html = template(context);
+      searchResult.html(html);
+    }
     $("#spinner").hide();
   });
 }
